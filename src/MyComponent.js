5ba056b58d4e431cae8d8f4931e0d7ea69845f00
“M-Hanan-Mustafa” <m.hananmustafa009@gmail.com>
2024-08-06 11:51:03 +0500

    Initial commit

---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+// src/MyComponent.js
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h2>Hello from MyComponent!</h2>
+    </div>
+  );
+};
+
+export default MyComponent;
